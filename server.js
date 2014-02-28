5b87fc234c3f0d72239f15e372e2c5deecc1445e
Ricky Chilcott <ricky@rbsavvy.com>
2014-02-27 23:04:27 -0500

    Cleanup code

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.startup(function() {
+    return Meteor.methods({
+      removeAllPosts: function() {
+        return Posts.remove({});
+      }
+    });
   });
 }
-
-Meteor.startup(function() {
-  return Meteor.methods({
-    removeAllPosts: function() {
-      return Posts.remove({});
-    }
-  });
-});
